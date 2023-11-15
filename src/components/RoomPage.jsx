2cc9c17b362eb2d1076a3630f54028cff1e5db67
Vishwajeet Yadav <vishwajeety14122@gmail.com>
2023-11-15 23:23:54 +0530

    Add stun server to iceServers array

--- src/components/RoomPage.jsx
+++ src/components/RoomPage.jsx
@@ -22,9 +22,11 @@ var pc; // Declare pc variable outside the block
       "https://rrturnserver.metered.live/api/v1/turn/credentials?apiKey=c6ff3a42c9063dc86cf2e8b90ff6e8c99b33"
     );
     const iceServers = await response.json();
-    configuration.iceServers = iceServers.unshift({
-      urls: "stun:stun.stunprotocol.org",
-    });
+    configuration.iceServers = [
+      {
+        urls: "stun:stun.stunprotocol.org",
+      },
+    ].concat(iceServers);
     console.log(configuration);
     pc = new RTCPeerConnection(configuration);
   } catch (error) {
